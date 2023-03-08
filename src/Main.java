de68c8cdd460546168b06881828cbf5c33ab84d8
JosiahElder <@JosiahElder>
2023-03-07 23:11:08 -0800

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import javax.swing.*;
 
-public class Main {
-
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         Student[] students = new Student[100];
         Staff[] staffs = new Staff[100];
         int studentCount = 0;
         int staffCount = 0;
         boolean finished = false;
 
-        while (!finished) {
+        while (!finished)
+        {
             String[] options = {"Student", "Staff", "Finish"};
             int choice = JOptionPane.showOptionDialog
                     (null,
@@ -18,12 +20,29 @@ public class Main {
                             JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
                             null, options, options[0]);
 
-            switch (choice) {
+            switch (choice)
+            {
                 case 0:
                     Student student = new Student();
                     student.setName(JOptionPane.showInputDialog("Enter Student Name"));
                     student.setAddress(JOptionPane.showInputDialog("Enter Student Address"));
                     String yearStr = JOptionPane.showInputDialog("Enter Student Year (1-4)");
+
+                    try {
+                        int year = Integer.parseInt(yearStr);
+                        if (year < 1 || year > 4) {
+                            JOptionPane.showMessageDialog(null,
+                                    "Invalid student year. Please enter a number between 1 and 4.");
+                            continue;
+                        }
+                        student.setYear(year);
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null,
+                                "Invalid input for student year. Please enter a number between 1 and 4.");
+                        continue;
+                    }
+                    students[studentCount] = student;
+                    studentCount++;
                     break;
                 case 1:
                     Staff staff = new Staff();
