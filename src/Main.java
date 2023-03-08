118507ea4f02f82286f942450d663ee35adfd3f3
JosiahElder <@JosiahElder>
2023-03-07 23:04:20 -0800

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student[] students = new Student[100];
+        Staff[] staffs = new Staff[100];
+        int studentCount = 0;
+        int staffCount = 0;
+        boolean finished = false;
+
+
     }
 }
