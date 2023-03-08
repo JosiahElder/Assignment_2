8cf8c37ef1dc5d83ea4fba1407ebd670cad7a041
JosiahElder <@JosiahElder>
2023-03-07 22:54:14 -0800

    Next Commit

--- src/Staff.java
+++ src/Staff.java
@@ -1,2 +1,43 @@
-public class Staff {
+class Staff
+{
+    private String name;
+    private String address;
+    private int yearsOfService;
+
+    public Staff()
+    {
+        this.name = "";
+        this.address = "";
+        this.yearsOfService = 0;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public void setYearsOfService(int yearsOfService)
+    {
+        this.yearsOfService = yearsOfService;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public int getYearsOfService()
+    {
+        return yearsOfService;
+    }
 }
