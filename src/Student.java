f864347aec7509fb27799e1e3f0c604b2c636442
JosiahElder <@JosiahElder>
2023-03-07 22:51:07 -0800

    Next Commit

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,35 @@
-public class Student {
+class Student {
+    private String name;
+    private String address;
+    private int year;
+
+    public Student() {
+        this.name = "";
+        this.address = "";
+        this.year = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
